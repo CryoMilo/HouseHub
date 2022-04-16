59b6bdec487f9a1f76260cbaec2c75dc81f0b7b6
Oak Soe Htoo Aung <minkojee@gmail.com>
2022-04-17 02:12:21 +0700

    added fb auth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,4 +1,9 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+	FacebookAuthProvider,
+	getAuth,
+	GoogleAuthProvider,
+	signInWithPopup,
+} from "firebase/auth";
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
@@ -12,6 +17,7 @@ const OAuth = () => {
 	const auth = getAuth();
 	const location = useLocation();
 
+	// Google Auth
 	const onGoogleLogin = async () => {
 		try {
 			const provider = new GoogleAuthProvider();
@@ -43,6 +49,39 @@ const OAuth = () => {
 		}
 	};
 
+	// Facebook Auth
+	const onFacebookLogin = async () => {
+		try {
+			const provider = new FacebookAuthProvider();
+			const result = await signInWithPopup(auth, provider);
+			const user = result.user;
+
+			// Check for user existance
+			const docRef = doc(db, "users", user.uid);
+			const docSnap = await getDoc(docRef);
+
+			// Check with docSnap
+			// If User doesn't exists
+			if (!docSnap.exists()) {
+				await setDoc(doc(db, "users", user.uid), {
+					name: user.displayName,
+					email: user.email,
+					timestamp: serverTimestamp(),
+				});
+				toast.success("Login Successful");
+				navigate("/");
+				// If user exists
+			} else {
+				toast.info("User already exists");
+				setTimeout(() => {
+					navigate("/profile");
+				}, 2000);
+			}
+		} catch (error) {
+			toast.error("An error has occured");
+		}
+	};
+
 	return (
 		<div className="flex gap-3 justify-center items-center mt-14">
 			<h3 className="text-left text-2xl text-white my-5">
@@ -51,7 +90,7 @@ const OAuth = () => {
 			<button className="bg-white rounded-full" onClick={onGoogleLogin}>
 				<GoogleIcon />
 			</button>
-			<button className="bg-white rounded-full">
+			<button className="bg-white rounded-full" onClick={onFacebookLogin}>
 				<FBIcon />
 			</button>
 		</div>
